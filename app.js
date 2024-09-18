5849820cd8858f1439bf30d835f2133c1eaa9d54
Toma Bogdan <bogdantoma2000@gmail.com>
2024-09-18 17:07:28 +0300

    add - path to home

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: ", PORT);
+});
